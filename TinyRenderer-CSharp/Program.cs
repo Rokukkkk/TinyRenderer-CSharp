b1e627a578029fb4da1f50bb08a13cc9e5ac9eb7
Rokukkkk <39435902+Rokukkkk@users.noreply.github.com>
2022-07-25 01:59:54 +0800

    Lesson 3

--- TinyRenderer-CSharp/Program.cs
+++ TinyRenderer-CSharp/Program.cs
@@ -8,27 +8,35 @@ namespace TinyRenderer_CSharp
 {
     class Program
     {
+        const int width = 2000;
+        const int height = 2000;
+
         static void Main(string[] args)
         {
             // Initiate frame
-            int width = 2000;
-            int height = 2000;
-            Image<Rgba32> image = new(width + 1, height + 1);  // Gird 1 to 800 only got 799 pixels
+            Image<Rgba32> image = new(width, height);  // Gird 1 to 800 only got 799 pixels
 
             // Load the model
             var model = Model.LoadModel(@"./obj/african_head.obj");
+            var texture = Texture.LoadTexture(@"./obj/african_head_diffuse.tga");
 
+            // Z-Buffer
+            float[] zBuffer = new float[width * height];
+            for (int i = 0; i < width * height; i++) zBuffer[i] = float.MinValue;
+
+            // Render
             Vector3 lightDir = new(0, 0, -1);
-            Vector2[] screenCoord = new Vector2[3];
+            Vector3[] screenCoord = new Vector3[3];
             Vector3[] worldCoord = new Vector3[3];
+            Vector2[] uv = new Vector2[3];
 
             foreach (var item in model)
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    Vector3 v = item[i].Vertex;
-                    screenCoord[i] = new Vector2((v.X + 1) * width / 2, (v.Y + 1) * height / 2);
-                    worldCoord[i] = v;
+                    worldCoord[i] = item[i].Vertex;
+                    screenCoord[i] = World2Screen(item[i].Vertex);
+                    uv[i] = item[i].UV;
                 }
 
                 Vector3 normal = Vector3.Cross(worldCoord[2] - worldCoord[0], worldCoord[1] - worldCoord[0]);
@@ -37,48 +45,19 @@ namespace TinyRenderer_CSharp
 
                 if (intensity > 0)
                 {
-                    byte rgb = (byte)(intensity * 255);
-                    DrawTriangle(screenCoord, ref image, new Rgba32(rgb, rgb, rgb, 255));
-                }
-
-                /*
-                // Draw all the triangles with random color
-                byte r = (byte)(new Random()).Next(255);  // Rgb value should be byte type
-                byte g = (byte)(new Random()).Next(255);
-                byte b = (byte)(new Random()).Next(255);
-                DrawTriangle(screenCoord, ref image, new Rgba32(r, g, b, 255));
-                */
-            }
-
-
-            /*
-            // Render the mesh lines
-            foreach (var item in model)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    Vector3 v0 = item[i].Vertex;
-                    Vector3 v1 = item[(i + 1) % 3].Vertex;
-                    int x0 = (int)((v0.X + 1) * width / 2);
-                    int y0 = (int)((v0.Y + 1) * height / 2);
-                    int x1 = (int)((v1.X + 1) * width / 2);
-                    int y1 = (int)((v1.Y + 1) * height / 2);
-
-                    DrawLine(x0, y0, x1, y1, ref image, Color.White);
+                    DrawTriangle(screenCoord, ref uv, ref zBuffer, ref image, ref texture, intensity);
                 }
             }
-            */
 
-
-            image.Mutate(x => x.RotateFlip(RotateMode.None, FlipMode.Vertical));  //Flip the image upside down
-
+            //Flip the image upside down, and saving
+            image.Mutate(x => x.RotateFlip(RotateMode.None, FlipMode.Vertical));
             SaveFrame(ref image);
 
         }
 
-        static void DrawTriangle(Vector2[] pts, ref Image<Rgba32> image, Rgba32 color)
+        static void DrawTriangle(Vector3[] pts, ref Vector2[] uv, ref float[] zBuffer, ref Image<Rgba32> image, ref Image<Rgba32> texture, float intensity)
         {
-            Vector2 bboxMin = new(image.Width - 1, image.Height - 1);
+            Vector2 bboxMin = new(width, height);
             Vector2 bboxMax = new(0, 0);
 
             bboxMin.X = (new float[] { bboxMin.X, pts[0].X, pts[1].X, pts[2].X }).Min();
@@ -86,77 +65,28 @@ namespace TinyRenderer_CSharp
             bboxMax.X = (new float[] { bboxMax.X, pts[0].X, pts[1].X, pts[2].X }).Max();
             bboxMax.Y = (new float[] { bboxMax.Y, pts[0].Y, pts[1].Y, pts[2].Y }).Max();
 
+
             for (int i = (int)bboxMin.X; i < bboxMax.X; i++)
             {
                 for (int j = (int)bboxMin.Y; j < bboxMax.Y; j++)
                 {
-                    Vector2 p = new(i, j);
+                    Vector3 p = new(i, j, 0);
                     Vector3 baryCoord = GetBarycentric(pts, p);
-                    if (baryCoord.X < 0 || baryCoord.Y < 0 || baryCoord.Z < 0) { continue; }
-                    image[(int)p.X, (int)p.Y] = color;
-                }
-            }
+                    Vector2 pUV;
+                    if (baryCoord.X < 0 || baryCoord.Y < 0 || baryCoord.Z < 0) continue;
 
-            /*
-            if (t0.Y == t1.Y && t0.Y == t2.Y) { return; }
-            // Sort 3 vertices based on Y
-            if (t0.Y > t1.Y) { Swap(ref t0, ref t1); }
-            if (t0.Y > t2.Y) { Swap(ref t0, ref t2); }
-            if (t1.Y > t2.Y) { Swap(ref t1, ref t2); }
-
-            int totalHeight = (int)(t2.Y - t0.Y);
-
-            for (int i = 0; i < totalHeight; i++)
-            {
-                bool secondHalf = i > t1.Y - t0.Y || t1.Y == t0.Y;
-                int segmentHeight = secondHalf ? (int)(t2.Y - t1.Y) : (int)(t1.Y - t0.Y);
-                float alpha = (float)i / totalHeight;
-                float beta = (float)(i - (secondHalf ? t1.Y - t0.Y : 0)) / segmentHeight;
-                Vector2 a = t0 + (t2 - t0) * alpha;
-                Vector2 b = secondHalf ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
-                if (a.X > b.X) { Swap(ref a, ref b); }
-                for (int j = (int)a.X; j <= b.X; j++)
-                {
-                    image[j, (int)t0.Y + i] = color;
-
-                }
-            }
-            */
-        }
-
-        // Bresenham’s Line Drawing Algorithm
-        static void DrawLine(int x0, int y0, int x1, int y1, ref Image<Rgba32> image, Rgba32 color)
-        {
-            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-            if (steep)
-            {
-                Swap(ref x0, ref y0);
-                Swap(ref x1, ref y1);
-            }
-            if (x0 > x1)
-            {
-                Swap(ref x0, ref x1);
-                Swap(ref y0, ref y1);
-            }
-
-            int dx = x1 - x0;
-            int dy = Math.Abs(y1 - y0);
-            int error = dx / 2;
-            int yStep = (y0 < y1) ? 1 : -1;
-            int y = y0;
-
-            for (int x = x0; x <= x1; x++)
-            {
-                if (steep)
-                    image[y, x] = color;
-                else
-                    image[x, y] = color;
-
-                error -= dy;
-                if (error < 0)
-                {
-                    y += yStep;
-                    error += dx;
+                    // Z-interpolation
+                    float zInterpolation = baryCoord.X * pts[0].Z + baryCoord.Y * pts[1].Z + baryCoord.Z * pts[2].Z;
+                    pUV = uv[0] * baryCoord.X + uv[1] * baryCoord.Y + uv[2] * baryCoord.Z;
+                    if (zInterpolation > zBuffer[(int)(p.X + p.Y * width)])
+                    {
+                        zBuffer[i + j * width] = zInterpolation;
+                        Rgba32 color = texture.GetColor(pUV);
+                        color.R = (byte)(color.R * intensity);
+                        color.G = (byte)(color.G * intensity);
+                        color.B = (byte)(color.B * intensity);
+                        image[i, j] = color;
+                    }
                 }
             }
         }
@@ -167,14 +97,8 @@ namespace TinyRenderer_CSharp
             image.SaveAsTga("./out.tga");
         }
 
-        // Swap two number
-        static void Swap<T>(ref T a, ref T b)
-        {
-            (b, a) = (a, b);
-        }
-
         // Calculate barycentric coordinates
-        static Vector3 GetBarycentric(Vector2[] pts, Vector2 p)
+        static Vector3 GetBarycentric(Vector3[] pts, Vector3 p)
         {
             float xa = pts[0].X;
             float ya = pts[0].Y;
@@ -190,6 +114,11 @@ namespace TinyRenderer_CSharp
             float alpha = 1 - gamma - beta;
 
             return new Vector3(alpha, beta, gamma);
+        }
+
+        static Vector3 World2Screen(Vector3 world)
+        {
+            return new Vector3((world.X + 1) * width / 2, (world.Y + 1) * height / 2, world.Z);
         }
     }
 }
---
+++ TinyRenderer-CSharp/Texture.cs
@@ -0,0 +1,39 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+using System.Numerics;
+
+namespace TinyRenderer_CSharp
+{
+    public static class Texture
+    {
+        public static Image<Rgba32> LoadTexture(string path)
+        {
+            var image = Image.Load(path);
+            image.Mutate(x => x.RotateFlip(RotateMode.None, FlipMode.Vertical));
+            return (Image<Rgba32>)image;
+        }
+
+
+        public static Rgba32 GetColor(this Image<Rgba32> image, Vector2 uv) =>
+            image[(int)(uv.X * image.Width), (int)(uv.Y * image.Height)];
+
+
+        public static Vector3 GetNormal(this Image<Rgba32> image, Vector2 uv)
+        {
+            var color = image[(int)(uv.X * image.Width), (int)(uv.Y * image.Height)];
+
+            return Vector3.Normalize(new Vector3(color.R / 225f * 2 - 1, color.G / 225f * 2 - 1, color.B / 225f * 2 - 1));
+        }
+
+
+        public static float GetSpecular(this Image<Rgba32> image, Vector2 uv) =>
+            image[(int)(uv.X * image.Width), (int)(uv.Y * image.Height)].R;
+
+
+        public static void SetColor(this Image<Rgba32> image, Vector2 uv, Rgba32 color) =>
+            image[(int)(uv.X * image.Width), (int)(uv.Y * image.Height)] = color;
+    }
+}
+
