f5130d0a448714921e18469f9dc60e5c8d35a6e7
Rokukkkk <nisemono_kakko@hotmail.com>
2022-07-31 12:09:44 +0800

    Fix depth map shader

--- TinyRenderer-CSharp/Shaders/Depth.cs
+++ TinyRenderer-CSharp/Shaders/Depth.cs
@@ -13,7 +13,9 @@ namespace TinyRenderer_CSharp.Shaders
 
         public Rgba32 GetFragment(ref FragmentPara para)
         {
-            Rgba32 color = new((byte)para.zInterpolation, (byte)para.zInterpolation, (byte)para.zInterpolation);
+            Vector3 p = Vector3.Normalize(para.worldCoord[0] * para.baryCoord.X + para.worldCoord[1] * para.baryCoord.Y + para.worldCoord[2] * para.baryCoord.Z);
+            float depth = (p.Z + 1) / 2;
+            Rgba32 color = new((byte)(255 * depth), (byte)(255 * depth), (byte)(255 * depth));
             return color;
         }
 
